4ea0d01d682b27b88ed1a5c611c287132313eced
stich <stavelka@gmail.com>
2017-08-14 02:36:35 +0300

    tabs added in created

--- src/Controllers/EntranceController.js
+++ src/Controllers/EntranceController.js
@@ -1,11 +1,10 @@
 app.controller('EntranceController', function($scope, $rootScope, $routeParams,$location, apiService){
      var that = this;
     var id = $routeParams.personId;
-    $scope.currentUser= $rootScope.currentUser;
-    // apiService.getPerson(id).then(function (response) {
-    //     $scope.currentUser = response.data;
-    //     $scope.currentUser.id = id;
-    // });
+    apiService.getPerson(id).then(function (response) {
+        $scope.currentUser = response.data;
+        $scope.currentUser.id = id;
+    });
 
     this.goAddRecipe= function()
     {
